c64dc224a95af445cb8f647e13152eae36a0472b
Ellana42 <mtmrkaploun@gmail.com>
2025-05-06 22:01:31 +0200

    router with all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'onda';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { HistoryComponent } from './history/history.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'accueil', component: HomeComponent },
+  { path: 'product', title: 'le produit', component: ProductComponent },
+  { path: 'product', title: 'le produit', component: ProductComponent },
+  { path: 'history', title: 'notre histoire', component: HistoryComponent },
+  { path: 'blog', title: 'blog', component: BlogComponent },
+  { path: 'contact', title: 'contact', component: ContactComponent },
+];
