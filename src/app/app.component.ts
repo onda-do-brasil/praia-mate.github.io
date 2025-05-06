41d755c43a9ce45af38600cf7ebc6bf2bebe3fd5
Ellana42 <mtmrkaploun@gmail.com>
2025-05-06 23:09:57 +0200

    first draft texts homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
 })
 export class AppComponent {
   title = 'onda';
