71f4870c6324bd98c9302e9b9a5b0fdc3c895f5f
Ellana42 <mtmrkaploun@gmail.com>
2025-05-15 22:40:53 +0200

    added contact page and improved styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage, CommonModule],
 })
 export class AppComponent {
+  constructor(public router: Router) {}
   title = 'onda';
+
+  shouldHideFooter(): boolean {
+    return '/contact' === this.router.url;
+  }
 }
